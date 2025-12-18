6263016b3338230aa6bcb469d448ac25cf035304
VIN7632 <158598182+VIN7632@users.noreply.github.com>
2025-12-18 17:21:32 +0300

    Update main.py

--- 3lab/main.py
+++ 3lab/main.py
@@ -1,4 +1,41 @@
+import os
+
+steps = []  # сюда сохраняем порядок обхода
 
 
-if __name__ == "__main__":
-    pass # Ваш код здесь
+def scan_folder(path):
+    info = {
+        "path": path,
+        "files": [],
+        "folders": []
+    }
+
+    steps.append(path)  # сохраняем шаг обхода
+
+    try:
+        items = os.listdir(path)
+    except:
+        return info
+
+    for name in items:
+        full_path = os.path.join(path, name)
+
+        if os.path.isdir(full_path):
+            folder_data = scan_folder(full_path)
+            info["folders"].append(folder_data)
+        else:
+            info["files"].append(name)
+
+    return info
+
+
+start_path = input("Введите путь к папке: ")
+result = scan_folder(start_path)
+
+print("\nПорядок обхода:")
+for s in steps:
+    print(s)
+
+print("\nСтруктура директорий:")
+print(result)
+
